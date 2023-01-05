b710698c4bbb3ab559b1e0dba7d42d2161b32b5b
Kim yeonsu <39877181+dustnehowl@users.noreply.github.com>
2023-01-05 19:19:45 +0900

    [Refactor] Test rename

--- src/main/java/com/example/test/repository/AuthRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.test.repository;
-
-import com.example.test.model.Test;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthRepository extends JpaRepository<Test, Long> {
-}
