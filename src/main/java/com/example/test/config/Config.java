e45b0be2e5a5f878ad43882ec3fb0c51c610098a
gim-yeonsu <dustnrkfnfn@naver.com>
2023-02-16 19:30:31 +0900

    [DOCKER] docker version up

--- src/main/java/com/example/test/config/Config.java
+++ src/main/java/com/example/test/config/Config.java
@@ -20,30 +20,30 @@ public class Config {
 
     private final FridgeRepository fridgeRepository;
 
-    @PostConstruct
-    public void init(){
-        try {
-            JSONParser parser = new JSONParser();
-            Reader reader = new FileReader("src/main/resources/static/fridge.json");
-            JSONObject jsonObject = (JSONObject) parser.parse(reader);
-
-            JSONArray jsonArray = (JSONArray) jsonObject.get("data");
-            int jsonSize = jsonArray.size();
-            for(int i=0; i<jsonSize; i++){
-                JSONObject jsonObject1 = (JSONObject) jsonArray.get(i);
-                String name = jsonObject1.get("name").toString();
-                String address = jsonObject1.get("address").toString();
-                JSONObject loc = (JSONObject) jsonObject1.get("loc");
-                double lat = (double) loc.get("lat");
-                double lng = (double) loc.get("lng");
-
-                Fridge fridge = fridgeRepository.findFridgeByAddress(address)
-                        .orElseGet(() -> fridgeRepository.save(new Fridge(name, address, lat, lng)));
-            }
-        }
-        catch(IOException | ParseException e)
-        {
-            throw new RuntimeException(e);
-        }
-    }
+//    @PostConstruct
+//    public void init(){
+//        try {
+//            JSONParser parser = new JSONParser();
+//            Reader reader = new FileReader("src/main/resources/static/fridge.json");
+//            JSONObject jsonObject = (JSONObject) parser.parse(reader);
+//
+//            JSONArray jsonArray = (JSONArray) jsonObject.get("data");
+//            int jsonSize = jsonArray.size();
+//            for(int i=0; i<jsonSize; i++){
+//                JSONObject jsonObject1 = (JSONObject) jsonArray.get(i);
+//                String name = jsonObject1.get("name").toString();
+//                String address = jsonObject1.get("address").toString();
+//                JSONObject loc = (JSONObject) jsonObject1.get("loc");
+//                double lat = (double) loc.get("lat");
+//                double lng = (double) loc.get("lng");
+//
+//                Fridge fridge = fridgeRepository.findFridgeByAddress(address)
+//                        .orElseGet(() -> fridgeRepository.save(new Fridge(name, address, lat, lng)));
+//            }
+//        }
+//        catch(IOException | ParseException e)
+//        {
+//            throw new RuntimeException(e);
+//        }
+//    }
 }
